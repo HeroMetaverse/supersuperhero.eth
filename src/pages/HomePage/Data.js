b64865f2b6fde599bbcbf1495d70b742486255ef
ubuntu <ubuntu@ubuntu.x>
2022-01-21 12:44:04 +0800

    save progress

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -4,14 +4,14 @@ export const homeObjOne = {
   lightTopLine: true,
   lightText: true,
   lightTextDesc: true,
-  topLine: ' ',
-  headline: 'The first DAO,based Metaverse',
+  topLine: '',
+  headline: 'The first DAO, based on Metaverse',
   description:
     "Allocate the world's largest DAO-directed treasury, partner with leading protocols, and build the future of finance.",    
   // buttonLabel: "string",
   imgStart: '',
   img: require('../../images/dao.jpg'),
-  alt: 'Credit Card',
+  alt: 'dao',
   start: ''
 };
 
@@ -21,14 +21,14 @@ export const homeObjTwo = {
   lightTopLine: true,
   lightText: true,
   lightTextDesc: true,
-  topLine: 'Instant Setup',
-  headline: 'Extremely quick onboarding process',
+  topLine: 'Long-term planned',
+  headline: 'Well-calculated economic model',
   description:
     "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
   buttonLabel: 'Learn More',
-  imgStart: '',
+  imgStart: 'start',
   img: require('../../images/metaverse.jpg'),
-  alt: 'Vault',
+  alt: 'Metaverse',
   start: ''
 };
 
@@ -44,7 +44,7 @@ export const homeObjThree = {
   description:
     "MetaverseDAO Link the wisdom of all sentient beings to realize great dreams.",
   buttonLabel: 'View Case Study',
-  imgStart: 'start',
+  imgStart: '',
   img: require('../../images/metaverse.jpg'),
   alt: 'Vault',
   start: 'true'
@@ -56,13 +56,12 @@ export const homeObjFour = {
   lightTopLine: true,
   lightText: true,
   lightTextDesc: true,
-  topLine: 'Secure Database',
-  headline: 'All your data is stored on our secure server',
-  description:
-    'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
-  buttonLabel: 'Sign Up Now',
+  topLine: 'efficient decision making',
+  headline: 'Loading',
+  description: 'Loading Loading Loading Loading Loading Loading Loading Loading Loading Loading ',
+  buttonLabel: 'Now',
   imgStart: 'start',
-  img: require('../../images/svg-3.svg'),
+  img: require('../../images/dao.jpg'),
   alt: 'Vault',
-  start: 'true'
+  start: ''
 };
--- src/pages/SuperMarket/Data.js
+++ src/pages/SuperMarket/Data.js
@@ -1,8 +1,8 @@
 export const homeObjOne = {
-    lightBg: true,
-    lightText: false,
-    lightTopLine: false,
-    lightTextDesc: false,
+    lightBg: false,
+    lightText: true,
+    lightTopLine: true,
+    lightTextDesc: true,
     topLine: 'View Our Products',
     headline: 'Shop through our catalog of products',
     description:
@@ -23,21 +23,21 @@ export const homeObjOne = {
     description:
       'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
     buttonLabel: 'Learn More',
-    imgStart: '',
+    imgStart: 'start',
     img: require('../../images/svg-1.svg'),
     alt: 'Vault'
   };
   
   export const homeObjThree = {
-    lightBg: true,
-    lightText: false,
-    lightTextDesc: false,
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
     topLine: 'Easy Setup',
     headline: 'Super fast and simple onboarding process',
     description:
       "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
     buttonLabel: 'Start Now',
-    imgStart: 'start',
+    imgStart: '',
     img: require('../../images/svg-1.svg'),
     alt: 'Vault'
   };
